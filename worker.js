9702609db2a1f3570c99ba87bdd1dd3beab56f8e
thaihunghung <110120031@st.tvu.edu.vn>
2024-11-26 21:02:28 +0700

    update

--- worker.js
+++ worker.js
@@ -1,19 +1,18 @@
 const PuppeteerAutoMetaMask = require('./puppeteer_auto_metamask/metamask_handler');
-const XLogin  = require('./auto_x/XLogin');
+const XLogin = require('./auto_x/XLogin');
 const { workerData, parentPort } = require('worker_threads');
 const puppeteerAutoMetaMask = PuppeteerAutoMetaMask(workerData.extensionPath, workerData.proxy);
 
 async function run() {
     const browser = await puppeteerAutoMetaMask.launchBrowser();
-    const page = await browser.newPage();
-    await page.goto('https://x.com/i/flow/login');
-    const X = workerData.dataX.split(' ');
     const address = workerData.testAddress;
     try {
         await new Promise(resolve => setTimeout(resolve, 10000));
         await puppeteerAutoMetaMask.interactWithMetaMask(browser, workerData.formattedMnemonic);
         //await XLogin.loginWithBackupCore(page, X)
-
+        // const page = await browser.newPage();
+        // await page.goto('https://x.com/i/flow/login');
+        // const X = workerData.dataX.split(' ');
 
 
         parentPort.postMessage({ status: 'Success', address });
